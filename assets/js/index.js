cab5d8ee3600039fe4f0b2e4b7b24eb87903f617
theobarretosilva <barretotheo25@gmail.com>
2022-08-03 18:05:04 -0300

    Atualizando projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,8 +1,5 @@
-import {mudarModalSucesso} from "../../modals/sucesso/sucesso.js";
+/*import {mudarModalSucesso} from "../../modals/sucesso/sucesso.js";
 import {modalSucesso} from "../../modals/sucesso/sucesso.js";
-
-
-
 
 const btnEnviar = document.getElementById('enviarDica');
 
@@ -21,4 +18,30 @@ window.onclick = function(event){
     if(event.targe == modalEditando){
         mudarModalEditando();
     }
+}*/
+    
+function cadastrarDica(){
+    const titulo = document.getElementById('titulo').value;
+    const linguagem = document.getElementById('linguagem').value;
+    const categoria = document.getElementById('inputCategoria').value;
+    const descricao = document.getElementById('descricao').value;
+    const linkYT = document.getElementById('inputLink').value;
+
+    var dados = JSON.parse(localStorage.getItem(titulo));
+
+    if(dados == null){
+        localStorage.setItem(titulo, "[]");
+        dados = [];
+    }
+    
+    const auxRegistro = {
+        titulo : titulo,
+        linguagem : linguagem,
+        categoria : categoria,
+        descricao : descricao,
+        linkYT : linkYT
+    }
+
+    dados.push(auxRegistro);
+    localStorage.setItem(titulo, JSON.stringify(dados));
 }
