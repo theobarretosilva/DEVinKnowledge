42d1455b299442697049cd05ae66e35716b7a593
theobarretosilva <barretotheo25@gmail.com>
2022-08-03 20:20:51 -0300

    Atualizando projeto

--- assets/js/index.js
+++ assets/js/index.js
@@ -19,22 +19,30 @@ window.onclick = function(event){
         mudarModalEditando();
     }
 }*/
-    
-function cadastrarDica(){
+
+const form = document.getElementById('formulario');
+form.onsubmit = cadastrarDica;
+
+document.body.onload = criarCard;
+
+function cadastrarDica(event){
+    event.preventDefault();
+
     const titulo = document.getElementById('titulo').value;
     const linguagem = document.getElementById('linguagem').value;
     const categoria = document.getElementById('inputCategoria').value;
     const descricao = document.getElementById('descricao').value;
     const linkYT = document.getElementById('inputLink').value;
 
-    var dados = JSON.parse(localStorage.getItem(titulo));
+    var dados = JSON.parse(localStorage.getItem("Dados"));
 
     if(dados == null){
-        localStorage.setItem(titulo, "[]");
+        localStorage.setItem("Dados", "[]");
         dados = [];
     }
     
     const auxRegistro = {
+        id : new Date().getTime(), 
         titulo : titulo,
         linguagem : linguagem,
         categoria : categoria,
@@ -43,8 +51,39 @@ function cadastrarDica(){
     }
 
     dados.push(auxRegistro);
-    localStorage.setItem(titulo, JSON.stringify(dados));
+    localStorage.setItem("Dados", JSON.stringify(dados));
 
-    const tituloLC = localStorage.titulo[descricao];
-    console.log(tituloLC);
+    //JSON.parse(localStorage.getItem("Dados")).filter(element => element.titulo == );
+    
+    criarCard();
+}
+
+function criarCard(){
+    const dados = JSON.parse(localStorage.getItem("Dados"));
+    const listaCards = document.getElementById('cardDicas');
+    let cards = "";
+
+    listaCards.innerHTML = "";
+
+    dados.forEach(element => {
+        cards += 
+        `<div class="dica">
+            <h1>${element.titulo}</h1>
+            <p><strong>Linguagem/Skill: </strong>${element.linguagem}</p>
+            <p><strong>Categoria: </strong>${element.categoria}</p>
+            <p>${element.descricao}</p>
+            <div class="botoesCard">
+                <button id="deletaDica"><img id="imgDeletaDica" src="./assets/imgs/deletaDica.png" alt="Botão para deletar dica"></button>
+                <button id="editaDica"><img id="imgEditaDica" src="./assets/imgs/editaDica.png" alt="Botão para editar dica"></button>
+                <button id="videoDica"><img id="imgVideoDica" src="./assets/imgs/videoDica.png" alt="Botão para ver o vídeo da dica"></button>
+            </div>
+            <p id="idElemento">${element.id}</p>
+        </div>`
+    });
+
+    listaCards.innerHTML = cards;
+}
+
+function deletaCard(){
+    
 }
